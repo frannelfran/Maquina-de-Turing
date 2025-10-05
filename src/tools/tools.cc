c4db5d7dc5a37709826a3e45ac8846c7c837a732
Franco Alla <frannelfran@gmail.com>
2025-10-05 21:11:55 +0100

    feat: Leer el fichero de entrada

---
+++ src/tools/tools.cc
@@ -0,0 +1,221 @@
+#include "tools.h"
+
+static Tools datos; // Variable global para almacenar los datos leídos
+
+/**
+ * @brief Dunción para leer el fichero de entrada y almacenar los datos en una estructura Tools
+ * @param nombreFichero Nombre del fichero de entrada
+ * @return Estructura Tools con los datos del fichero
+ */
+Tools leerFichero(const string& nombreFichero) {
+  ifstream file(nombreFichero);
+
+  if (!file.is_open()) { // Comprobar la lectura del fichero
+    throw runtime_error("No se pudo abrir el fichero: " + nombreFichero);
+  }
+
+  string linea;
+  // Saltar los comentarios y líneas vacías
+  while (getline(file, linea)) {
+    if (linea.empty() || linea[0] == '#') {
+      continue;
+    }
+    break;
+  }
+  // Leo los estados
+  leerEstados(istringstream(linea));
+
+  // Leo el alfabeto de entrada
+  getline(file, linea);
+  leerAlfabeto(istringstream(linea));
+
+  // Leo el alfabeto de la cinta
+  getline(file, linea);
+  leerAlfabeto(istringstream(linea));
+
+  // Leo el estado inicial
+  getline(file, linea);
+  comprobarEstado(linea);
+  for (Estado* estado : datos.estados) {
+    if (estado->getId() == linea) {
+      estado->setInicial();
+      break;
+    }
+  }
+
+  // Leo el conjunto de estados finales
+  getline(file, linea);
+  leerEstadosFinales(istringstream(linea));
+
+  // Leo el simbolo blanco de la máquina
+  getline(file, linea);
+  comprobarSimbolo(linea[0]);
+
+  // Leo las transiciones
+  int id = 1;
+  while (getline(file, linea)) {
+    if (linea.empty() || linea[0] == '#') {
+      continue;
+    }
+    leerTransiciones(istringstream(linea), id++);
+  }
+  return datos;
+}
+
+/**
+ * @brief Función para leer el conjunto de estados
+ * @param is Stream de entrada
+ * @return void
+ */
+void leerEstados(istringstream is) {
+  string linea;
+  set<Estado*> estados;
+  while (is >> linea) {
+    estados.insert(new Estado(linea));
+  }
+  datos.estados = estados;
+}
+
+/**
+ * @brief Funci´pn para leer los estados finales del fichero
+ * @param is Stream de entrada
+ * @return void
+ */
+void leerEstadosFinales(istringstream is) {
+  string linea;
+  while (is >> linea) {
+    comprobarEstado(linea);
+    for (Estado* estado : datos.estados) {
+      if (estado->getId() == linea) {
+        estado->setAceptacion();
+        break;
+      }
+    }
+  }
+}
+
+/**
+ * @brief Función para leer los alfabetos del fichero
+ * @param is Stream de entrada
+ * @return void
+ */
+void leerAlfabeto(istringstream is) {
+  string linea;
+  Alfabeto alfabeto;
+  while (is >> linea) {
+    alfabeto.insertar(linea[0]);
+  }
+  if (datos.alfabetos.first.size() == 0) {
+    datos.alfabetos.first = alfabeto;
+  } else {
+    datos.alfabetos.second = alfabeto;
+  }
+}
+
+/**
+ * @brief Función para leer las transiciones del fichero
+ * @param is Stream de entrada
+ * @param id Identificador de la transición
+ * @return void
+ */
+void leerTransiciones(istringstream is, int id) {
+  string actual, lecturaCinta, siguiente, escrituraCinta, movimientoCinta;
+  is >> actual >> lecturaCinta >> siguiente >> escrituraCinta >> movimientoCinta;
+  // Compruebo los estados
+  comprobarEstado(actual), comprobarEstado(siguiente);
+  // Compruebo los símbolos
+  comprobarSimbolo(lecturaCinta[0]), comprobarSimbolo(escrituraCinta[0]), comprobarSimbolo(movimientoCinta[0]);
+  // Busco el estaddo inicial y siguiente en el conjunto de estados
+  Estado* estadoSiguiente = buscarEstado(siguiente), *estadoActual = buscarEstado(actual);
+  // Creo la transición y la agrego la transicion
+  Transicion transicion(id, estadoActual, lecturaCinta[0], estadoSiguiente, escrituraCinta[0], movimientoCinta[0]);
+  for (Estado* e : datos.estados) {
+    if (e->getId() == actual) {
+      e->agregarTransicion(transicion);
+      break;
+    }
+  }
+}
+
+/**
+ * @brief Método para buscar un estado en el conjunto de estados
+ * @param estado Estado a buscar
+ * @return Puntero al estado si se encuentra, nullptr en caso contrario
+ */
+Estado* buscarEstado(const string& estado) {
+  for (Estado* e : datos.estados) {
+    if (e->getId() == estado) {
+      return e;
+    }
+  }
+  return nullptr;
+}
+
+
+/**
+ * @brief Función para combrobar que los estados pertencen al conjunto de estados
+ * @param actual Estado actual
+ * @param siguiente Estado siguiente
+ * @return void
+ */
+void comprobarEstado(const string& estado) {
+  bool encontrado = false;
+  for (Estado* e : datos.estados) {
+    if (e->getId() == estado) {
+      encontrado = true;
+    }
+  }
+
+  if (!encontrado) {
+    cerr << "Q -> {";
+    for (auto it = datos.estados.begin(); it != datos.estados.end(); ++it) {
+      cerr << (*it)->getId();
+      if (next(it) != datos.estados.end()) {
+        cerr << ", ";
+      }
+    }
+    cerr << "}" << endl;
+    throw runtime_error("El estado " + estado + " no está en el conjunto de estados.");
+  }
+}
+
+/**
+ * @brief Función para comprobar que el símbolo pertenece al alfabeto del autómata
+ * @param simbolo Símbolo a comprobar
+ * @return void
+ */
+void comprobarSimbolo(const char& simbolo) {
+  bool pertenece = false;
+  bool escrituraLectura = false;
+
+  if (simbolo == '.') { // epsilon siempre pertenece
+    return;
+  } else if (datos.alfabetos.first.pertenece(simbolo)) {
+    pertenece = true;
+  } else if (datos.alfabetos.second.pertenece(simbolo)) {
+    pertenece = true;
+  } else if (simbolo == 'E' || simbolo == 'L') { // Movimientos de la cinta siempre pertenecen
+    escrituraLectura = true;
+  }
+
+  if (!pertenece) {
+    cerr << "Σ -> " << datos.alfabetos.first << endl;
+    cerr << "Γ -> " << datos.alfabetos.second << endl;
+    throw runtime_error("El símbolo '" + string(1, simbolo) + "' no pertenece a ningún alfabeto.");
+  } else if (!escrituraLectura) {
+    throw runtime_error("El símbolo '" + string(1, simbolo) + "' no es un símbolo de escritura/lectura válido.");
+  }
+}
+
+/**
+ * @brief Función para mostrar la cabecera de la traza
+ * @return void
+ */
+void mostrarCabecera() {
+  cout << "------------------------------------------------------------" << endl;
+  cout << left;
+  cout << setw(15) << "Estado actual" 
+  << setw(15) << "Cinta"
+  << setw(15) << "Transiciones" << endl;
+  cout << "------------------------------------------------------------" << endl;
+}
---
+++ src/tools/tools.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "../alfabeto/alfabeto.h"
+#include "../transicion/transicion.h" // Incluir primero Transicion
+#include "../estado/estado.h"         // Luego Estado
+#include <fstream> // Para manejo de archivos
+#include <stdexcept> // Para las excepciones
+#include <sstream> // Para istringstream
+
+#ifndef TOOLS_H
+#define TOOLS_H
+
+/**
+ * @struct Tools
+ * @brief Estructura para almacenar los datos del fichero de entrada
+ */
+struct Tools {
+  pair<Alfabeto, Alfabeto> alfabetos;
+  set<Estado*> estados;
+};
+
+Tools leerFichero(const string& nombreFichero); // Leo el fichero para obtener los datos
+void leerEstados(istringstream is); // Leo los estados del fichero
+void leerEstadosFinales(istringstream is); // Leo los estados finales del fichero
+void leerAlfabeto(istringstream is); // Leo el alfabeto del fichero
+void leerTransiciones(istringstream is, int id); // Leo las transiciones del fichero
+void comprobarEstado(const string& estado); // Compruebo que el estado es válido
+void comprobarSimbolo(const char& simbolo); // Compruebo que el símbolo es válido
+Estado* buscarEstado(const string& estado); // Buscar un estado en el conjunto de estados
+void mostrarCabecera(); // Muestro el inicio de la traza
+
+#endif // TOOLS_H
