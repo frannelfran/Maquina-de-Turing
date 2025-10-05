4bfd8204a67e88014276f0f2b9ea77f3d9a56e50
Franco Alla <frannelfran@gmail.com>
2025-10-05 21:21:06 +0100

    feat: Declaración de la clase Automata

---
+++ src/automata/automata.cc
@@ -0,0 +1,141 @@
+#include "automata.h"
+
+/**
+ * @brief Constructor de la clase Automata
+ * @param estados Conjunto de estados del autómata
+ * @param alfabetoEntrada Alfabeto de entrada del autómata
+ * @param alfabetoPila Alfabeto de la pila del autómata
+ * @param topPila Símbolo inicial de la pila
+ */
+Automata::Automata(const set<Estado*>& estados, const Alfabeto& alfabetoEntrada, const Alfabeto& alfabetoCinta) {
+  estados_ = estados;
+  alfabetoEntrada_ = alfabetoEntrada;
+  alfabetoCinta_ = alfabetoCinta;
+
+  // Inicializo el estado actual al estado inicial
+  for (Estado* estado : estados_) {
+    if (estado->esInicial()) {
+      estadoActual_ = estado;
+      break;
+    }
+  }
+}
+
+/**
+ * @brief Método para ejecutar el autómata con una cadena de entrada
+ * @param cadena Cadena de entrada
+ * @return true si la cadena es aceptada, false en caso contrario
+ */
+bool Automata::ejecutar(string cadena) {
+  return true;
+}
+
+/**
+ * @brief Método para obtener las transiciones posibles desde el estado actual
+ * @param simbolo Cadena de entrada
+ * @param transicionesUsadas Vector de transiciones usadas (no utilizado en la nueva implementación)
+ * @return Vector de transiciones posibles
+ */
+vector<Transicion*> Automata::obtenerTransicionesPosibles(char simbolo, vector<Transicion*> transicionesUsadas) {
+  vector<Transicion*> transicionesPosibles;
+  
+  for (auto& transicion : estadoActual_->getTransiciones()) {
+    if (transicion.getLecturaCinta() == simbolo || transicion.getLecturaCinta() == '.') {
+      transicionesPosibles.push_back(&transicion);
+    }
+  }
+  
+  return transicionesPosibles;
+}
+
+/**
+ * @brief Método para mostrar la traza de la ejecución del autómata
+ * @param cadena Cadena de entrada
+ * @param transiciones Vector de transiciones disponibles
+ * @return void
+ */
+//void Automata::mostrarTraza(const string& cadena, const vector<Transicion*>& transiciones) {
+//  cout << left
+//  << setw(15) << estadoActual_->getId()
+//  << setw(15) << (cadena.empty() ? "-" : cadena)
+//  << setw(15) << (pilaStr.empty() ? "-" : pilaStr);
+//
+//  if (transiciones.empty()) {
+//    // Miro si es la transición final
+//    if (cadena.empty() && pila_.empty()) {
+//      cout << setw(15) << "w∈L";
+//    } else {
+//      cout << setw(15) << "∄";
+//    }
+//    cout << "\n------------------------------------------------------------" << endl;
+//    return;
+//  }
+//
+//  // Mostrar las transiciones posibles
+//  for (auto it = transiciones.begin(); it != transiciones.end(); ++it) {
+//    cout << left
+//    << setw(0) << (*it)->getId();
+//    if (next(it) != transiciones.end()) {
+//      cout << ", ";
+//    }
+//  }
+//  cout << "\n------------------------------------------------------------" << endl;
+//}
+
+/**
+ * @brief Método para reiniciar el autómata a su estado inicial
+ * @return void
+ */
+void Automata::reiniciar() {
+  // Reinicio el estado actual al estado inicial
+  for (Estado* estado : estados_) {
+    if (estado->esInicial()) {
+      estadoActual_ = estado;
+      break;
+    }
+  }
+}
+
+/**
+ * @brief Método para comprobar si una cadena es válida para el autómata
+ * @param cadena Cadena de entrada
+ * @return true si la cadena es válida, false en caso contrario
+ */
+bool Automata::esValida(const string& cadena) const {
+  for (char simbolo : cadena) {
+    if (!alfabetoEntrada_.pertenece(simbolo) && simbolo != '.') {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * @overload Sobrecarga del operador << para mostrar el autómata
+ */
+ostream& operator<<(ostream& os, const Automata& automata) {
+  os << "Q -> {";
+  for (auto it = automata.estados_.begin(); it != automata.estados_.end(); ++it) {
+    os << (*it)->getId();
+    if (next(it) != automata.estados_.end()) {
+      os << ", ";
+    }
+  }
+  os << "}" << endl;
+  os << "Σ -> " << automata.alfabetoEntrada_ << endl;
+  os << "Γ -> " << automata.alfabetoCinta_ << endl;
+  os << "q0 -> " << automata.estadoActual_->getId() << endl;
+  os << "F -> {";
+  for (auto estado : automata.estados_) {
+    if (estado->esAceptacion()) {
+      os << estado->getId() << ", ";
+    }
+  }
+  os << "}" << endl;
+  
+  // Transiciones
+  for (Estado* estado : automata.estados_) {
+    os << *estado << endl;
+  }
+  return os;
+}
---
+++ src/automata/automata.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "../alfabeto/alfabeto.h"
+#include "../transicion/transicion.h"
+#include "../estado/estado.h"
+#include <iomanip> // Para std::setw
+#include <vector> // Para std::vector
+#include <algorithm> // Para reverse
+
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+
+/**
+ * @class Automata
+ * @brief Clase que representa un autómata
+ */
+class Automata {
+  public:
+    // Constructor y destructor
+    Automata(const set<Estado*>& estados, const Alfabeto& alfabetoEntrada, const Alfabeto& alfabetoCinta);
+    ~Automata() = default;
+
+    // Sobrecarga de operadores
+    friend ostream& operator<<(ostream& os, const Automata& automata);
+
+    // Métodos
+    bool ejecutar(string cadena);
+    bool esValida(const string& cadena) const;
+    void reiniciar();
+    void mostrarTraza(const string& cadena, const vector<Transicion*>& transiciones) {}
+    vector<Transicion*> obtenerTransicionesPosibles(char simbolo, vector<Transicion*> transicionesUsadas);
+
+  private:
+    set<Estado*> estados_;
+    Alfabeto alfabetoEntrada_;
+    Alfabeto alfabetoCinta_;
+    Cinta cinta_;
+    Estado* estadoActual_;
+};
+
+#endif // AUTOMATA_H
